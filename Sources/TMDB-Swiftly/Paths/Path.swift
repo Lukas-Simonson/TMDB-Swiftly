bda728ce4fdc5e5685687b1801f3ee1625ac8430
Lukas Simonson <lukas@hydradesignllc.com>
2022-09-27 17:27:56 -0600

    Added API Path options

---
+++ Sources/TMDB-Swiftly/Paths/Path.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lukas Simonson on 9/27/22.
+//
+
+import Foundation
+
+extension TMDBSwiftly {
+    
+    enum Path: CustomStringConvertible {
+        case company
+        case configuration
+        case credits
+        case discover
+        case find
+        case genre
+        case guestSession
+        case keyword
+        case list
+        case movie
+        case network
+        case trending
+        case person
+        case review
+        case search
+        case watchProviders
+        
+        var description: String {
+            return ""
+        }
+    }
+}
