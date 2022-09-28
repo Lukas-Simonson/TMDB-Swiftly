21c5ebb0239c9959b19d83212842ec53e8d54752
Lukas Simonson <lukas@hydradesignllc.com>
2022-09-28 12:29:45 -0600

    Adds Documentation to the Certification Endpoints and Path

--- Sources/TMDB-Swiftly/Endpoints/CertificationEndpoints.swift
+++ Sources/TMDB-Swiftly/Endpoints/CertificationEndpoints.swift
@@ -11,13 +11,24 @@ import HydraSwiftExtensions
 extension TMDBSwiftly.Certification {
     
     // MARK: Movie Certifications
+    
+    /// Retrieves Movie Certification data and converts it to a TSCertification object.
+    ///
+    /// - Parameters:
+    ///   - key: An API key to access TMDB.
+    ///
     public static func getMovieTSCertifications( key: String ) async throws -> TSCertifications {
         return try await getMovieCertifications(key: key)
     }
     
+    /// Retrieves Movie Certification data and converts it to the given `Codable` object.
+    ///
+    /// - Parameters:
+    ///   - key: An API key to access TMDB.
+    ///
     public static func getMovieCertifications< Cert: Codable >( key: String ) async throws -> Cert {
+        
         let urlString = "\(TMDBSwiftly.basePath)\(TMDBSwiftly.APIVersion.v3)/certification/movie/list?\(TMDBSwiftly.QueryParameter.apiKey)\(key)"
-        print( urlString )
         guard let url = URL(string: urlString) else {
             throw TMDBSwiftly.TSError.couldntGenerateURL(from: urlString )
         }
@@ -26,13 +37,24 @@ extension TMDBSwiftly.Certification {
     }
     
     // MARK: TV Certifications
+    
+    /// Retrieves TV Certification data and converts it to a TSCertification object.
+    ///
+    /// - Parameters:
+    ///   - key: An API key to access TMDB.
+    ///
     public static func getTVTSCertifications( key: String ) async throws -> TSCertifications {
         return try await getTVCertifications(key: key)
     }
     
+    /// Retrieves TV Certification data and converts it to the given `Codable` object.
+    ///
+    /// - Parameters:
+    ///   - key: An API key to access TMDB.
+    ///
     public static func getTVCertifications< Cert: Codable >( key: String ) async throws -> Cert {
-        let urlString = "\(TMDBSwiftly.basePath)\(TMDBSwiftly.APIVersion.v3)/certification/tv/list?\(TMDBSwiftly.QueryParameter.apiKey)\(key)"
         
+        let urlString = "\(TMDBSwiftly.basePath)\(TMDBSwiftly.APIVersion.v3)/certification/tv/list?\(TMDBSwiftly.QueryParameter.apiKey)\(key)"
         guard let url = URL(string: urlString) else {
             throw TMDBSwiftly.TSError.couldntGenerateURL(from: urlString )
         }
--- Sources/TMDB-Swiftly/Models/Certifications/CertificationModel.swift
+++ Sources/TMDB-Swiftly/Models/Certifications/CertificationModel.swift
@@ -7,13 +7,22 @@
 
 import Foundation
 
+/// A Wrapper that handles the given Certification data.
 public struct TSCertifications: Codable {
     
+    /// All Different Certifications Keyed by Country Code.
     public var certifications: [ String : [ Certification ] ]
     
+    /// A Representation of the Certification aka Rating of the given Media.
     public struct Certification: Codable {
+        
+        /// The name of the certification.
         public var certification: String
+        
+        /// What the certification means.
         public var meaning: String
+        
+        /// The order of 'severity' of the certification.
         public var order: Int
     }
 }
