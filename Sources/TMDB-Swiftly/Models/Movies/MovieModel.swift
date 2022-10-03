5b8685d066bf8c1d9ce87bdbd13cb70f191bf7a9
Lukas Simonson <lukas@hydradesignllc.com>
2022-10-03 14:42:31 -0600

    Adds Coding Keys to all Movie Models

--- Sources/TMDB-Swiftly/Models/Movies/MovieModel.swift
+++ Sources/TMDB-Swiftly/Models/Movies/MovieModel.swift
@@ -35,22 +35,68 @@ public struct TSMovie: Codable {
     let voteAverage: Double?
     let voteCount: Double?
     
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case belongsToCollection = "belongs_to_collection"
+        case budget
+        case genres
+        case homepage
+        case id
+        case imdbID = "imdb_id"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview
+        case popularity
+        case posterPath = "poster_path"
+        case productionCompanies = "production_companies"
+        case productionCountries = "production_countries"
+        case releaseDate = "release_date"
+        case revenue
+        case runtime
+        case spokenLanguages = "spoken_languages"
+        case status
+        case tagline
+        case title
+        case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+    
     struct MovieCollection: Codable {
         let id: Int?
         let name: String?
         let posterPath: String?
         let backdropPath: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case id
+            case name
+            case posterPath = "poster_path"
+            case backdropPath = "backdrop_path"
+        }
     }
     
     struct SpokenLanguage: Codable {
         let countryCode: String
         let englishName: String
         let name: String
+        
+        enum CodingKeys: String, CodingKey {
+            case countryCode = "iso_639_1"
+            case englishName = "english_name"
+            case name
+        }
     }
     
     struct ProductionCountry: Codable {
         let countryCode: String
         let name: String
+        
+        enum CodingKeys: String, CodingKey {
+            case countryCode = "iso_3166_1"
+            case name
+        }
     }
     
     struct Genre: Codable {
