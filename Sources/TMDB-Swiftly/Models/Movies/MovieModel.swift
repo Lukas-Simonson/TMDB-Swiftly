59a321325dd6387a7f3d82248cd42f27e58ede76
Lukas Simonson <lukas@hydradesignllc.com>
2022-10-04 01:49:19 -0600

    Makes Movie Properties Public

--- Sources/TMDB-Swiftly/Models/Movies/MovieModel.swift
+++ Sources/TMDB-Swiftly/Models/Movies/MovieModel.swift
@@ -9,31 +9,31 @@ import Foundation
 
 public struct TSMovie: Codable {
     
-    let adult: Bool?
-    let backdropPath: String?
-    let belongsToCollection: MovieCollection?
-    let budget: Int?
-    let genres: [ Genre ]?
-    let homepage: String?
-    let id: Int
-    let imdbID: String?
-    let originalLanguage: String?
-    let originalTitle: String?
-    let overview: String?
-    let popularity: Double?
-    let posterPath: String?
-    let productionCompanies: [ TSCompany ]?
-    let productionCountries: [ ProductionCountry ]?
-    let releaseDate: String?
-    let revenue: Int?
-    let runtime: Int?
-    let spokenLanguages: [ SpokenLanguage ]?
-    let status: String?
-    let tagline: String?
-    let title: String?
-    let video: Bool?
-    let voteAverage: Double?
-    let voteCount: Double?
+    public let adult: Bool?
+    public let backdropPath: String?
+    public let belongsToCollection: MovieCollection?
+    public let budget: Int?
+    public let genres: [ Genre ]?
+    public let homepage: String?
+    public let id: Int
+    public let imdbID: String?
+    public let originalLanguage: String?
+    public let originalTitle: String?
+    public let overview: String?
+    public let popularity: Double?
+    public let posterPath: String?
+    public let productionCompanies: [ TSCompany ]?
+    public let productionCountries: [ ProductionCountry ]?
+    public let releaseDate: String?
+    public let revenue: Int?
+    public let runtime: Int?
+    public let spokenLanguages: [ SpokenLanguage ]?
+    public let status: String?
+    public let tagline: String?
+    public let title: String?
+    public let video: Bool?
+    public let voteAverage: Double?
+    public let voteCount: Double?
     
     enum CodingKeys: String, CodingKey {
         case adult
@@ -64,10 +64,10 @@ public struct TSMovie: Codable {
     }
     
     public struct MovieCollection: Codable {
-        let id: Int?
-        let name: String?
-        let posterPath: String?
-        let backdropPath: String?
+        public let id: Int?
+        public let name: String?
+        public let posterPath: String?
+        public let backdropPath: String?
         
         enum CodingKeys: String, CodingKey {
             case id
@@ -78,9 +78,9 @@ public struct TSMovie: Codable {
     }
     
     public struct SpokenLanguage: Codable {
-        let countryCode: String
-        let englishName: String
-        let name: String
+        public let countryCode: String
+        public let englishName: String
+        public let name: String
         
         enum CodingKeys: String, CodingKey {
             case countryCode = "iso_639_1"
@@ -90,9 +90,9 @@ public struct TSMovie: Codable {
     }
     
     public struct ProductionCountry: Codable {
-        let countryCode: String
-        let name: String
-        
+        public let countryCode: String
+        public let name: String
+
         enum CodingKeys: String, CodingKey {
             case countryCode = "iso_3166_1"
             case name
@@ -100,16 +100,16 @@ public struct TSMovie: Codable {
     }
     
     public struct Genre: Codable {
-        let id: Int
-        let name: String
+        public let id: Int
+        public let name: String
     }
     
     public struct TSMovieResults: Codable {
-        let page: Int?
-        let results: [ TSMovie ]?
-        let dates: Dates?
-        let totalPages: Int?
-        let totalResults: Int?
+        public let page: Int?
+        public let results: [ TSMovie ]?
+        public let dates: Dates?
+        public let totalPages: Int?
+        public let totalResults: Int?
         
         enum CodingKeys: String, CodingKey {
             case page
